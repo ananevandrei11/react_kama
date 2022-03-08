70e8f2f2a41e9175e5cf1dbc9196a50535ed44c3
Andrei Ananev <andrei.ananev2@simbirsoft.com>
2022-03-08 19:12:57 +0400

    add users api

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,69 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
+import * as axios from "axios";
+import userNoAvatar from "../../Asets/images/noavatar.svg";
 
 const Users = (props) => {
-  console.log(props.users.length);
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        avatarURL: '',
-        fullName: 'Andrei',
-        status: 'I am web-developer',
-        location: { city: 'Samara', country: 'Russia' },
-        followed: false
-      },
-      {
-        id: 2,
-        avatarURL: '',
-        fullName: 'Sergei',
-        status: 'I am backend',
-        location: { city: 'Orsk', country: 'Russia' },
-        followed: true
-      },
-      {
-        id: 3,
-        avatarURL: '',
-        fullName: 'Victor',
-        status: 'I am frontend',
-        location: { city: 'Moscow', country: 'Russia' },
-        followed: true
-      },
-      {
-        id: 4,
-        avatarURL: '',
-        fullName: 'Marina',
-        status: 'I am C++',
-        location: { city: 'SPb', country: 'Russia' },
-        followed: false
-      },
-    ])
+  let btnGetUser = useRef(null);
+
+  let getUsers = () => {
+    let myUrl = 'https://test.aniganweb.ru/testapi/testusers.json';
+    let samuraiUrl = 'https://social-network.samuraijs.com/api/1.0/users'
+    axios.get(samuraiUrl)
+      .then(function (response) {
+        props.setUsers(response.data.items);
+        btnGetUser.current.style.display = 'none';
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
-  console.log(props.users);
+
   return (
     <div>
+      <button
+        onClick={() => getUsers()}
+        ref={btnGetUser}
+      >Get Users</button>
       {
         props.users.map((user) =>
           <article key={user.id.toString()}>
             <div>
               <figure>
                 <img
-                  width="20"
-                  height="20"
-                  src={user.avatarURL}
+                  width="40"
+                  height="40"
+                  src={
+                    (user.photos.small != null)
+                      ? user.photos.small : userNoAvatar
+                  }
                   alt=" "
-                  style={{backgroundColor: "red"}}
+                  style={{ backgroundColor: "red" }}
                 />
               </figure>
-              {user.followed ? <button
-                onClick={() => { props.unFollow(user.id) }}>Follow</button> : <button
-                  onClick={() => { props.follow(user.id) }}>Unfollow</button>}
+              {user.followed
+                ?
+                <button
+                  onClick={() => { props.unFollow(user.id) }}
+                >Follow</button>
+                :
+                <button
+                  onClick={() => { props.follow(user.id) }}
+                >Unfollow</button>}
 
             </div>
             <div>
-              <p>{user.fullName}</p>
+              <p>{user.name}</p>
               <p>{user.status}</p>
-              <p>{user.location.country}</p>
-              <p>{user.location.city}</p>
+              <p>{"user.location.country"}</p>
+              <p>{"user.location.city"}</p>
             </div>
           </article>
         )
